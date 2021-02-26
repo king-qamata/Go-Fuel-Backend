0968751a085547a7ca79c29b4fe753bba08ff9a1
King Qamata <petey.olu@gmail.com>
2021-02-26 10:16:42 +0100

    first look api+swagge & SQL database

--- app/app/settings.py
+++ app/app/settings.py
@@ -91,22 +91,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'HOST': os.environ.get('DB_HOST'),
-        # 'HOST': 'app-f63cb26d-c373-4e6f-956b-1740b00989c2-do-user-6168075-0.b.db.ondigitalocean.com',
-         'NAME': os.environ.get('DB_NAME'),
-        # 'NAME': 'db',
-         'USER': os.environ.get('DB_USER'),
-        # 'USER': 'db',
-         'PASSWORD': os.environ.get('DB_PASS'),
-        # 'PASSWORD': 'cz3yfwnv97t8azjr',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    #'default': {
+      #   'ENGINE': 'django.db.backends.postgresql',
+       #  'HOST': os.environ.get('DB_HOST'),
+        # 'HOST': 'app-f63cb26d-c373-4e6f-956b-1740b00989c2-do-user-6168075-0.b.db.ondigitalocean.com',
+       #  'NAME': os.environ.get('DB_NAME'),
+        # 'NAME': 'db',
+        # 'USER': os.environ.get('DB_USER'),
+        # 'USER': 'db',
+         #'PASSWORD': os.environ.get('DB_PASS'),
+        # 'PASSWORD': 'cz3yfwnv97t8azjr',
+  #}
 
     #host     : app-f63cb26d-c373-4e6f-956b-1740b00989c2-do-user-6168075-0.b.db.ondigitalocean.com
 #port     : 25060
